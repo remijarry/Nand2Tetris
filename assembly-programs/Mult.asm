881cf4b8bd865d8c44923675ec3b2771397c14ed
remijarry <rem_ay@hotmail.fr>
2024-07-02 16:33:01 +1200

    multiplication with negative numbers handling.

--- assembly-programs/Mult.asm
+++ assembly-programs/Mult.asm
@@ -22,12 +22,38 @@ D;JEQ
 D=M
 @END
 D;JEQ
-D=D-1 // remove 1 so when the loop stops at 0 we have not added one more R1 to the result
+
+
+// we copy r0 and r1 somewhere else in the memory, this will serve later to apply the correct sign to the result.
+@R0
+D=M
+@copy_r0
 M=D
 
+@R1
+D=M
+@copy_r1
+M=D
+
+//set r0 and r1 to their absolute value
+@R0
+D=M
+@ABS_R0
+D;JLT
+
+// we come back to this line after having set up r0 to its absolute value
+(NEXT_ABS)
+@R1
+D=M
+@ABS_R1
+D;JLT
+
+
+(MAIN)
 // store the result in R2
 @R2
-M=0
+M=0 // initialise to 0 (might not be required)
+@R1
 // Loop R1 times and add R1 to result
 (LOOP)
  @R2
@@ -38,11 +64,67 @@ M=0
  M=D // update result
  @R1
  D=M
- MD=D-1
- @END
- D;JEQ
+ MD=D-1 // decrement R1.
+ @FIX_RESULT_SIGN
+ D;JEQ // if we've reached 0, then we are done with the multiplication, we now need to adjust the sign.
  @LOOP
  0;JMP
 
+
+// if one of the input was negative, change the sign of the result
+(FIX_RESULT_SIGN)
+  @copy_r0
+  D=M
+  @R0_IS_NEGATIVE
+  D;JLT
+
+  // If (r0 > 0 && r1 < 0) then negate_result
+  @copy_r1
+  D=M
+  @NEGATE_RESULT
+  D;JLT
+
+  @END
+  0;JMP
+
+(R0_IS_NEGATIVE)
+  // if R1 is also negative, result is already positive, we exit
+  @copy_r1
+  D=M
+  @END
+  D;JLT
+  @NEGATE_RESULT
+  D;JGT
+
+(NEGATE_RESULT)
+  // we negate the result and exit.
+  @R2
+  D=M
+  D=!D
+  D=D+1
+  M=D
+  @END
+  0;JMP
+
 (END)
+  @END
+  0;JMP
+
+
+// absolute values
+(ABS_R0)
+  @R0
+  D=M
+  D=!D
+  D=D+1
+  M=D
+  @NEXT_ABS
+  0;JMP
+(ABS_R1)
+  @R1
+  D=M
+  D=!D
+  D=D+1
+  M=D
+  @MAIN
   0;JMP
