6156a2ffbd03ad8927deed573b6074dfe9f2ce10
remijarry <rem_ay@hotmail.fr>
2024-07-06 08:49:39 +1200

    VMTranslator project + unit test created.

---
+++ vm-translator/src/VMTranslator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VMTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
