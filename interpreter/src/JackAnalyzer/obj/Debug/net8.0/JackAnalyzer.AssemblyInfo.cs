b193da0481693fc22fb30c2830cd3110cb920bd9
remijarry <=>
2025-03-05 20:48:00 +1300

    gitignore file moved.

--- interpreter/src/JackAnalyzer/obj/Debug/net8.0/JackAnalyzer.AssemblyInfo.cs
+++ interpreter/src/JackAnalyzer/obj/Debug/net8.0/JackAnalyzer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JackAnalyzer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f3a46c7d465f0ec8918defdce358aabe4d18f4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffce68623589b3e960de5392629b95ca889aea03")]
 [assembly: System.Reflection.AssemblyProductAttribute("JackAnalyzer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JackAnalyzer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
